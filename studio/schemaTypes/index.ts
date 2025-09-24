1fd2118a2743c2a9a20a5399c751f7d75c90636d
Ilyas Nassiri <nassiriilyase@gmail.com>
2025-09-25 00:18:32 +0200

    ok

--- studio/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-// studio/schemaTypes/index.ts
-import villa from "./villa";
-import activity from "./activity";
-import galleryImage from "./galleryImage";
-
-// 👉 on exporte un **nommé** "schemaTypes"
-export const schemaTypes = [villa, activity, galleryImage];
